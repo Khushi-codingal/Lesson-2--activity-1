8bc1b9d62faf281e745e63b2d4fdc9cd22b0f7be
Khushi-codingal <rashmiabc@gmail.com>
2025-10-30 17:56:07 +0530

    lesson 2 activity 2

---
+++ activity2.py
@@ -0,0 +1,12 @@
+#creating different variables
+x = 5
+y = "Johan"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+#user input
+name = input("enter your name: ")
+print("\nhello", name, "\nwelcome to my world")
